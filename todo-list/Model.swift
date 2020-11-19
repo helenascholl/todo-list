f609dfd01d4e0383f16737da66ce01fa1b8ec889
Administrator <administrator@Joes-Mac.local>
2020-11-19 09:49:25 +0100

    Initial Commit

---
+++ todo-list/Model.swift
@@ -0,0 +1,14 @@
+class ToDo {
+    
+    var id = 0
+    var userId = 0
+    var title = ""
+    var completed = false
+    
+}
+
+class Model {
+    
+    var todos = [ToDo]()
+    
+}
---
+++ todo-list/TableViewController.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let queue = DispatchQueue(label: "queue1")
+    var model = Model()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        queue.async {
+            self.download()
+        }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.todos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> TableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todo", for: indexPath) as! TableViewCell
+        let todo = model.todos[indexPath.row]
+        
+        cell.textLabel?.text = todo.title
+        cell.detailTextLabel?.text = todo.completed ? "Completed" : "Not completed"
+        cell.button.setTitle("\(todo.userId)", for: .normal)
+
+        return cell
+    }
+    
+    func download() {
+        let todoModel = Model()
+        
+        if let url = URL(string: "http://jsonplaceholder.typicode.com/todos") {
+            if let data = try? Data(contentsOf: url) {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []), let array = json as? [Any] {
+                    for obj in array {
+                        if let dict = obj as? [String: Any] {
+                            let todo = ToDo()
+                            
+                            todo.title = dict["title"] as! String
+                            todo.id = dict["id"] as! Int
+                            todo.userId = dict["userId"] as! Int
+                            todo.completed = dict["completed"] as! Bool
+                            
+                            todoModel.todos.append(todo)
+                        }
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.model = todoModel
+                        self.tableView.reloadData()
+                    }
+                }
+            } else {
+                print("Download failed")
+            }
+        } else {
+            print("Cannot resolve URL")
+        }
+        
+    }
+
+}
